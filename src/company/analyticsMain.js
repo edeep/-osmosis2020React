f480730b138bdda490ef075765f79ff0c0320cb0
Balaji Sridharan <balaji.sridharan.ext@nokia.com>
2020-03-08 17:51:27 +0530

    added reports

--- src/company/analyticsMain.js
+++ src/company/analyticsMain.js
@@ -1,33 +1,144 @@
 import React from 'react';
-import Highcharts from 'highcharts'
-import HighchartsReact from 'highcharts-react-official'
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+import { URL } from '../sharedComponents/constants';
 
 export default class AnalyticsMain extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            value: 0
+            value: 0,
+            chartSelected: 'select',
+            report: {},
+            highChartOption: {},
+            reportURLS: {
+                monthlySubcriptions: 'manufacturer/monthlySubscriptionsReport',
+                monthlySubcriptionsPerSubscription: 'manufacturer/monthlySubscriptionsPerSubscriptionReport',
+                select: null
+            }
         }
     }
 
-    options = {
-        title: {
-            text: 'Testing chart....'
-        },
-        series: [{
-            data: [1, 2, 3]
-        }]
+    onChartSelection = event => {
+        const chartSelected = event.target.value;
+        if (this.state.reportURLS[chartSelected] === null) {
+            this.setState(state => {
+                let updatedState = {...state};
+                updatedState.highChartOption = {};
+                updatedState.chartSelected = chartSelected;
+                return updatedState;
+            });
+        } else {
+            this.getReport(chartSelected);
+        }
+    };
+
+    getReport = chartSelected => {
+        let url = URL.concat(this.state.reportURLS[chartSelected]);
+        return fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw Error(response.status);
+                }
+                return response;
+            })
+            .then(
+                response => {
+                    console.log('Came to Fetch Result ');
+                    response.json().then(data => {
+                        console.log('fetched data', data);
+                        if (chartSelected === 'monthlySubcriptions') {
+                            this.getMonthlySubscribers(data, chartSelected);
+                        } else if (chartSelected === 'monthlySubcriptionsPerSubscription') {
+                            this.getMonthlySubscribersBySubscription(data, chartSelected);
+                        }
+                    });
+                })
+            .catch(error => {console.log('Error ', error)});
     }
+
+    getMonthlySubscribers = (data, chartSelected) => {
+        this.setState(state => {
+            let updatedState = { ...state };
+            updatedState.highChartOption = {
+                title: {
+                    text: 'Number of Montly Subscriptions'
+                },
+                xAxis: {
+                    categories: data.categories
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: 'Number of Subscriptions'
+                    }
+                },
+                series: data.data
+            };
+            updatedState.chartSelected = chartSelected;
+            return updatedState;
+        });
+    }
+
+    getMonthlySubscribersBySubscription = (data, chartSelected) => {
+        this.setState(state => {
+            let updatedState = { ...state };
+            updatedState.highChartOption = {
+                chart: {
+                    type: 'column'
+                },
+                title: {
+                    text: 'Number of Montly Subscriptions by Subscription'
+                },
+                xAxis: {
+                    categories: data.categories
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: 'Number of Subscriptions'
+                    }
+                },
+                tooltip: {
+                    headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                    pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                        '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+                    footerFormat: '</table>',
+                    shared: true,
+                    useHTML: true
+                },
+                plotOptions: {
+                    column: {
+                        pointPadding: 0.2,
+                        borderWidth: 0
+                    }
+                },
+                series: data.data
+            };
+            updatedState.chartSelected = chartSelected;
+            return updatedState;
+        });
+    }
+
 
     render() {
         return (
             <div>
-             
-                 <HighchartsReact
-                    highcharts={Highcharts}
-                    options={this.options}
-                />
+
+                <form>
+                    <select value={this.state.chartSelected} onChange={this.onChartSelection}>
+                        <option value="select">Select</option>
+                        <option value="monthlySubcriptions">Number of Montly Subscriptions</option>
+                        <option value="monthlySubcriptionsPerSubscription">Number of Montly Subscriptions by Subscription</option>
+                    </select>
+                </form>
+
+                {Object.keys(this.state.highChartOption).length !== 0 &&
+                    <HighchartsReact
+                        highcharts={Highcharts}
+                        options={this.state.highChartOption}
+                    />}
 
             </div>);
     }
